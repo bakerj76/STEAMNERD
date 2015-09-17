8b4a43cce2612df1a86cdf28c62c818db23f771b
James <jamesbakeratech@gmail.com>
2015-09-16 19:55:40 -0700

    Started a Login class to do logging in stuff

---
+++ Login.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using SteamKit2;
+
+namespace SteamNerd
+{
+    class Login
+    {
+        private SteamNerd _steamNerd;
+        private string _username;
+        private string _password;
+        private string _authCode;
+        private string _twoFactorCode;
+
+        public Login(SteamNerd steamNerd, CallbackManager manager)
+        {
+            _steamNerd = steamNerd;
+
+            manager.Subscribe<SteamClient.ConnectedCallback>(OnConnect);
+            manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnect);
+            manager.Subscribe<SteamUser.UpdateMachineAuthCallback>(OnMachineAuth);
+            manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
+        }
+
+        public void Connect()
+        {
+            Console.WriteLine("Connecting to Steam...");
+            _steamNerd.SteamClient.Connect();
+        }
+
+        public void OnConnect(SteamClient.ConnectedCallback callback)
+        {
+            // Some error popped up while trying to log in
+            if (callback.Result != EResult.OK)
+            {
+                Console.WriteLine("{0}", callback.Result);
+
+                //_steamNerd.IsRunning = false;
+                return;
+            }
+
+            byte[] sentryHash = null;
+
+            if (File.Exists("sentry.bin"))
+            {
+                var sentryFile = File.ReadAllBytes("sentry.bin");
+                sentryHash = CryptoHelper.SHAHash(sentryFile);
+            }
+
+            _steamNerd.SteamUser.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = _username,
+                Password = _password,
+
+                AuthCode = _authCode,
+                TwoFactorCode = _twoFactorCode,
+                SentryFileHash = sentryHash,
+            });
+        }
+
+        public void OnDisconnect(SteamClient.DisconnectedCallback callback)
+        {
+
+        }
+
+        public void OnMachineAuth(SteamUser.UpdateMachineAuthCallback callback)
+        {
+            Console.WriteLine("Updating sentryfile...");
+
+            // write out our sentry file
+            // ideally we'd want to write to the filename specified in the callback
+            // but then this sample would require more code to find the correct sentry file to read during logon
+            // for the sake of simplicity, we'll just use "sentry.bin"
+
+            int fileSize;
+            byte[] sentryHash;
+            using (var fs = File.Open("sentry.bin", FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                fs.Seek(callback.Offset, SeekOrigin.Begin);
+                fs.Write(callback.Data, 0, callback.BytesToWrite);
+                fileSize = (int)fs.Length;
+
+                fs.Seek(0, SeekOrigin.Begin);
+                using (var sha = new SHA1CryptoServiceProvider())
+                {
+                    sentryHash = sha.ComputeHash(fs);
+                }
+            }
+
+            // inform the steam servers that we're accepting this sentry file
+            _steamNerd.SteamUser.SendMachineAuthResponse(new SteamUser.MachineAuthDetails
+            {
+                JobID = callback.JobID,
+
+                FileName = callback.FileName,
+
+                BytesWritten = callback.BytesToWrite,
+                FileSize = fileSize,
+                Offset = callback.Offset,
+
+                Result = EResult.OK,
+                LastError = 0,
+
+                OneTimePassword = callback.OneTimePassword,
+
+                SentryFileHash = sentryHash,
+            }
+            );
+
+            Console.WriteLine("Done!");
+        }
+
+        private void OnLoggedOn(SteamUser.LoggedOnCallback callback)
+        {
+            var isSteamGuard = callback.Result == EResult.AccountLogonDenied;
+            var is2FA = callback.Result == EResult.AccountLoginDeniedNeedTwoFactor;
+
+            if (isSteamGuard || is2FA)
+            {
+                Console.WriteLine("This account is SteamGuard protected!");
+
+                if (is2FA)
+                {
+                    Console.Write("Please enter your 2 factor auth code from your authenticator app: ");
+                    _twoFactorCode = Console.ReadLine();
+                }
+                else
+                {
+                    Console.Write("Please enter the auth code sent to the email at {0}: ", callback.EmailDomain);
+                    _authCode = Console.ReadLine();
+                }
+
+                return;
+            }
+
+            if (callback.Result != EResult.OK)
+            {
+                Console.WriteLine("Unable to logon to Steam: {0} / {1}", callback.Result, callback.ExtendedResult);
+
+                //_steamNerd.IsRunning = false;
+                return;
+            }
+
+            //Console.WriteLine("Logged in!");
+            _password = "";
+        }
+    }
+}
