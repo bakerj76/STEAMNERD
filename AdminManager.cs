3a2823ed2e823c90da327e887a5d80fb79b0b7f5
James <jamesbakeratech@gmail.com>
2015-09-22 08:43:01 -0700

    Started AdminManager

---
+++ AdminManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamNerd
+{
+    class AdminManager
+    {
+        public List<Module> AdminModules;
+        // public List<User> Admins;
+
+        public void CheckCommand(string[] args)
+        {
+
+        }
+
+        public void AddModule(Module module)
+        {
+            AdminModules.Add(module);
+        }
+    }
+}
