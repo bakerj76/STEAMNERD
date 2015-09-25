0f09bddf5dcf06c5e3c9124abd49f8f6ffd1ab5c
James <jamesbakeratech@gmail.com>
2015-09-24 20:44:37 -0700

    Added Admin saving and loading

--- AdminManager.cs
+++ AdminManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,20 +13,52 @@ namespace SteamNerd
         private string _adminListPath;
         public List<SteamID> Admins;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="adminListPath">The file that contains the admin list.</param>
+        public AdminManager(string adminListPath)
+        {
+            _adminListPath = adminListPath;
+        }
+
+        /// <summary>
+        /// Adds an admin and gives them permissions.
+        /// </summary>
+        /// <param name="user">The user to make admin.</param>
         public void AddAdmin(User user)
         {
             user.IsAdmin = true;
             Admins.Add(user.SteamID);
         }
 
+        /// <summary>
+        /// Save the list of admins.
+        /// </summary>
         public void Save()
         {
-
+            using (var file = new StreamWriter(_adminListPath))
+            {
+                foreach (var admin in Admins)
+                {
+                    file.Write(admin.Render() + "\r\n");
+                }
+            }
         }
 
         public void Load(string adminListPath)
         {
-            _adminListPath = adminListPath;
+            using (var file = new StreamReader(_adminListPath))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.Trim();
+
+                    var steamID = new SteamID(line);
+                    Admins.Add(steamID);
+                }
+            }
         }
     }
 }
