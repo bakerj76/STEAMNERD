8352af9adf86fcd742a8cfb1c0a21db668909037
Mark Samman <mark.samman@gmail.com>
2015-10-06 20:08:22 +0200

    Fix more pep8 violations

--- Modules/PersistentChat.py
+++ Modules/PersistentChat.py
@@ -1,14 +1,16 @@
-clr.AddReference("System.Core")
 import System
+
+clr.AddReference("System.Core")
 clr.ImportExtensions(System.Linq)
 
 Module.Name = "Persistent Chat"
 Module.Description = "Message xXxTrollSlayerxXx to get invited to the chat."
 Module.Global = True
 
+
 def OnFriendMessage(callback, args):
-	if len(SteamNerd.Chatrooms) == 0:
-		return
-	
-	chat = SteamNerd.Chatrooms.Keys.First()
-	SteamNerd.SteamFriends.InviteUserToChat(callback.Sender, chat)
+    if len(SteamNerd.Chatrooms) == 0:
+        return
+
+    chat = SteamNerd.Chatrooms.Keys.First()
+    SteamNerd.SteamFriends.InviteUserToChat(callback.Sender, chat)
